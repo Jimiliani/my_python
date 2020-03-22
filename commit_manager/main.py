8c105f56ce378867cf62b84bdf9b561d8a9e79dd
Jimiliani <dikorolyov@mail.ru>
2020-03-22 14:16:43 +0300

    Убиты маленькие баги

--- commit_manager/main.py
+++ commit_manager/main.py
@@ -17,14 +17,18 @@ def main():
         line = line.strip('\n').split(' ')
         repo = g.get_repo(line[3])
         row = []
+        last_date = deadline_date_from
         total_commits = 0
         for commit in repo.get_commits():
             date = GithubDate(str(commit.commit.author.date))
             if deadline_date_from < date < deadline_date_to:
                 total_commits += 1
-                row = [line[0], line[1], line[2], "Всего коммитов: " + str(total_commits),
-                       'https://github.com/' + line[3], str(commit.commit.message)]
-        csv_writer(csv_file, row)
+                if last_date < date:
+                    last_date = date
+                    row = [line[0], line[1], line[2], "Всего коммитов: " + str(total_commits),
+                           'https://github.com/' + line[3], str(commit.commit.message)]
+        if row:
+            csv_writer(csv_file, row)
     file.close()
     csv_file.close()
 
