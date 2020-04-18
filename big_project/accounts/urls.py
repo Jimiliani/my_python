26bfe05e92cb26200e7b3537d655cb0a73cc5e3d
Jimiliani <dikorolyov@mail.ru>
2020-04-18 11:57:13 +0300

    Ура! Теперь есть регистрация и вход, только входить пока некуда, главной страницы нет) О красоте позабочусь когда будет весь функционал

---
+++ big_project/accounts/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+
+from .views import *
+
+app_name = 'accounts'
+urlpatterns = [
+    path('register/', register, name='register'),
+    path('login/', login, name='login'),
+]
